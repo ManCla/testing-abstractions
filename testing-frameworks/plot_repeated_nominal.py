03646687821b0c5adcb31e290ad5bef3676a5c0b
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-03-07 15:34:59 +0100

    aestetical improvements and comments to code

--- testing-frameworks/plot_repeated_nominal.py
+++ testing-frameworks/plot_repeated_nominal.py
@@ -4,20 +4,17 @@ import numpy as np
 from plot.Plot import Storage
 import matplotlib.pyplot as plt
 
+# directory of the repeated tests
 directory = 'pitl/flightdata/nominal-repeated'
+# length of traces
 trace_length = 10000
 
 if __name__ == "__main__":
+  
+  #######################
+  ### initializations ###
+  #######################
 
-  # parsing command line parameters
-  # if len(sys.argv) != 3 or sys.argv[1] not in ("pdf", "show"):
-  #   print('\033[91mError:\033[0m ' + 'python plot.py <1> <2>')
-  #   print('  <1>: either "pdf" or "show"')
-  #   print('  <2>: absolute or relative path of the experiment file')
-  #   exit()
-
-  # command = sys.argv[1]
-  # file_location = sys.argv[2]
   experiment_names = os.listdir(directory)
   experiment_names.sort()
   num_tests        = len(experiment_names)
@@ -39,6 +36,10 @@ if __name__ == "__main__":
   min_plot.estimated_position_z = 1000*np.ones(trace_length)
   min_plot.time = np.linspace(1, trace_length, num=trace_length)
 
+  #######################
+  ### data extraction ###
+  #######################
+
   ## open all flight data 
   all_data = [Storage() for i in range(num_tests)]
   for i in range(num_tests):
@@ -48,6 +49,7 @@ if __name__ == "__main__":
 
   ## iterate over time to get average min and max
   for t in range(trace_length):
+    ## iterate over the different tests
     for data in range(num_tests):
       ## average values
       avg_plot.estimated_position_x[t] = avg_plot.estimated_position_x[t] + \
@@ -71,10 +73,14 @@ if __name__ == "__main__":
       max_plot.estimated_position_z[t] = max(max_plot.estimated_position_z[t], \
                                              all_data[data].estimated_position_z[t])
 
-  # get some setpoints
+  # get setpoints
   avg_plot.setpoint_position_x = all_data[0].data.set_pt[0, 0:trace_length]
   avg_plot.setpoint_position_y = all_data[0].data.set_pt[1, 0:trace_length]
   avg_plot.setpoint_position_z = all_data[0].data.set_pt[2, 0:trace_length]
+
+  #######################
+  ### actual plotting ###
+  #######################
 
   # some settingss
   chosen_size = (20, 7)
@@ -85,28 +91,28 @@ if __name__ == "__main__":
   fig, axs = plt.subplots(3, 1, figsize=chosen_size)
   plt.subplots_adjust(wspace=0.2, hspace=1)
 
-  axs[0].title.set_text('Position (x)')
+  axs[0].title.set_text('Estimated Position (x)')
   axs[0].plot(avg_plot.time, avg_plot.setpoint_position_x, 'k')
-  axs[0].plot(min_plot.time, min_plot.estimated_position_x, 'b:')
-  axs[0].plot(avg_plot.time, avg_plot.estimated_position_x, 'b')
-  axs[0].plot(max_plot.time, max_plot.estimated_position_x, 'b:')
-  axs[0].legend(['min', 'avg', 'max'])
+  axs[0].plot(min_plot.time, min_plot.estimated_position_x, 'b', linestyle = 'dashed')
+  axs[0].plot(avg_plot.time, avg_plot.estimated_position_x, 'b:')
+  axs[0].plot(max_plot.time, max_plot.estimated_position_x, 'b', linestyle = 'dashed')
+  axs[0].legend(['setpoint','min', 'avg', 'max'])
   axs[0].grid(color=chosen_grid_color, linestyle=chosen_grid_linestyle, linewidth=chosen_grid_linewidth)
 
-  axs[1].title.set_text('Position (y)')
+  axs[1].title.set_text('Estimated Position (y)')
   axs[1].plot(avg_plot.time, avg_plot.setpoint_position_y, 'k')
-  axs[1].plot(min_plot.time, min_plot.estimated_position_y, 'g:')
-  axs[1].plot(avg_plot.time, avg_plot.estimated_position_y, 'g')
-  axs[1].plot(max_plot.time, max_plot.estimated_position_y, 'g:')
-  axs[1].legend(['min', 'avg', 'max'])
+  axs[1].plot(min_plot.time, min_plot.estimated_position_y, 'g', linestyle = 'dashed')
+  axs[1].plot(avg_plot.time, avg_plot.estimated_position_y, 'g:')
+  axs[1].plot(max_plot.time, max_plot.estimated_position_y, 'g', linestyle = 'dashed')
+  axs[1].legend(['setpoint','min', 'avg', 'max'])
   axs[1].grid(color=chosen_grid_color, linestyle=chosen_grid_linestyle, linewidth=chosen_grid_linewidth)
 
-  axs[2].title.set_text('Position (z)')
+  axs[2].title.set_text('Estimated Position (z)')
   axs[2].plot(avg_plot.time, avg_plot.setpoint_position_z, 'k')
-  axs[2].plot(min_plot.time, min_plot.estimated_position_z, 'r:')
-  axs[2].plot(avg_plot.time, avg_plot.estimated_position_z, 'r')
-  axs[2].plot(max_plot.time, max_plot.estimated_position_z, 'r:')
-  axs[1].legend(['min', 'avg', 'max'])
+  axs[2].plot(min_plot.time, min_plot.estimated_position_z, 'r', linestyle = 'dashed')
+  axs[2].plot(avg_plot.time, avg_plot.estimated_position_z, 'r:')
+  axs[2].plot(max_plot.time, max_plot.estimated_position_z, 'r', linestyle = 'dashed')
+  axs[2].legend(['setpoint','min', 'avg', 'max'])
   axs[2].grid(color=chosen_grid_color, linestyle=chosen_grid_linestyle, linewidth=chosen_grid_linewidth)
 
   plt.show()
