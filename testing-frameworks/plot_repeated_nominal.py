df0c6487a5124d2e568ac587870d0b3e98fba713
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-03-07 20:47:31 +0100

    added option to create csv for repeated nominal plot

--- testing-frameworks/plot_repeated_nominal.py
+++ testing-frameworks/plot_repeated_nominal.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 directory = 'pitl/flightdata/nominal-repeated'
 # length of traces
 trace_length = 10000
+# generate csv files or not
+gen_csv = True
+csv_name = 'repeated_nominal.csv'
 
 if __name__ == "__main__":
   
@@ -24,17 +27,14 @@ if __name__ == "__main__":
   avg_plot.estimated_position_x = np.zeros(trace_length)
   avg_plot.estimated_position_y = np.zeros(trace_length)
   avg_plot.estimated_position_z = np.zeros(trace_length)
-  avg_plot.time = np.linspace(1, trace_length, num=trace_length)
   max_plot = Storage()
   max_plot.estimated_position_x = np.zeros(trace_length)
   max_plot.estimated_position_y = np.zeros(trace_length)
   max_plot.estimated_position_z = np.zeros(trace_length)
-  max_plot.time = np.linspace(1, trace_length, num=trace_length)
   min_plot = Storage()
   min_plot.estimated_position_x = 1000*np.ones(trace_length)
   min_plot.estimated_position_y = 1000*np.ones(trace_length)
   min_plot.estimated_position_z = 1000*np.ones(trace_length)
-  min_plot.time = np.linspace(1, trace_length, num=trace_length)
 
   #######################
   ### data extraction ###
@@ -73,6 +73,9 @@ if __name__ == "__main__":
       max_plot.estimated_position_z[t] = max(max_plot.estimated_position_z[t], \
                                              all_data[data].estimated_position_z[t])
 
+  # get time trace
+  avg_plot.time = all_data[0].time[0:trace_length]
+
   # get setpoints
   avg_plot.setpoint_position_x = all_data[0].data.set_pt[0, 0:trace_length]
   avg_plot.setpoint_position_y = all_data[0].data.set_pt[1, 0:trace_length]
@@ -93,27 +96,53 @@ if __name__ == "__main__":
 
   axs[0].title.set_text('Estimated Position (x)')
   axs[0].plot(avg_plot.time, avg_plot.setpoint_position_x, 'k')
-  axs[0].plot(min_plot.time, min_plot.estimated_position_x, 'b', linestyle = 'dashed')
+  axs[0].plot(avg_plot.time, min_plot.estimated_position_x, 'b', linestyle = 'dashed')
   axs[0].plot(avg_plot.time, avg_plot.estimated_position_x, 'b:')
-  axs[0].plot(max_plot.time, max_plot.estimated_position_x, 'b', linestyle = 'dashed')
+  axs[0].plot(avg_plot.time, max_plot.estimated_position_x, 'b', linestyle = 'dashed')
   axs[0].legend(['setpoint','min', 'avg', 'max'])
   axs[0].grid(color=chosen_grid_color, linestyle=chosen_grid_linestyle, linewidth=chosen_grid_linewidth)
 
   axs[1].title.set_text('Estimated Position (y)')
   axs[1].plot(avg_plot.time, avg_plot.setpoint_position_y, 'k')
-  axs[1].plot(min_plot.time, min_plot.estimated_position_y, 'g', linestyle = 'dashed')
+  axs[1].plot(avg_plot.time, min_plot.estimated_position_y, 'g', linestyle = 'dashed')
   axs[1].plot(avg_plot.time, avg_plot.estimated_position_y, 'g:')
-  axs[1].plot(max_plot.time, max_plot.estimated_position_y, 'g', linestyle = 'dashed')
+  axs[1].plot(avg_plot.time, max_plot.estimated_position_y, 'g', linestyle = 'dashed')
   axs[1].legend(['setpoint','min', 'avg', 'max'])
   axs[1].grid(color=chosen_grid_color, linestyle=chosen_grid_linestyle, linewidth=chosen_grid_linewidth)
 
   axs[2].title.set_text('Estimated Position (z)')
   axs[2].plot(avg_plot.time, avg_plot.setpoint_position_z, 'k')
-  axs[2].plot(min_plot.time, min_plot.estimated_position_z, 'r', linestyle = 'dashed')
+  axs[2].plot(avg_plot.time, min_plot.estimated_position_z, 'r', linestyle = 'dashed')
   axs[2].plot(avg_plot.time, avg_plot.estimated_position_z, 'r:')
-  axs[2].plot(max_plot.time, max_plot.estimated_position_z, 'r', linestyle = 'dashed')
+  axs[2].plot(avg_plot.time, max_plot.estimated_position_z, 'r', linestyle = 'dashed')
   axs[2].legend(['setpoint','min', 'avg', 'max'])
   axs[2].grid(color=chosen_grid_color, linestyle=chosen_grid_linestyle, linewidth=chosen_grid_linewidth)
 
   plt.show()
 
+  if gen_csv :
+    with open(csv_name, 'w') as f:
+
+      # header for file
+      f.write('i, time, ' +
+              'min_estimated_position_x, min_estimated_position_y, min_estimated_position_z, ' +
+              'avg_estimated_position_x, avg_estimated_position_y, avg_estimated_position_z, ' +
+              'max_estimated_position_x, max_estimated_position_y, max_estimated_position_z, ' +
+              'setpoint_position_x, setpoint_position_y, setpoint_position_z, \n')
+
+      for i in range(trace_length):
+        ith_string = str(i) + ", " + \
+                     str(avg_plot.time[i]) + ", " + \
+                     str(min_plot.estimated_position_x[i]) + ", " + \
+                     str(min_plot.estimated_position_y[i]) + ", " + \
+                     str(min_plot.estimated_position_z[i]) + ", " + \
+                     str(avg_plot.estimated_position_x[i]) + ", " + \
+                     str(avg_plot.estimated_position_y[i]) + ", " + \
+                     str(avg_plot.estimated_position_z[i]) + ", " + \
+                     str(max_plot.estimated_position_x[i]) + ", " + \
+                     str(max_plot.estimated_position_y[i]) + ", " + \
+                     str(max_plot.estimated_position_z[i]) + ", " + \
+                     str(avg_plot.setpoint_position_x[i])  + ", " + \
+                     str(avg_plot.setpoint_position_y[i])  + ", " + \
+                     str(avg_plot.setpoint_position_z[i])  + "\n"
+        f.write(ith_string)
