08fec084db4b114594e06ec749529bec0bc1426f
ManCla <claudio.mandrioli@gmail.com>
2023-01-23 09:50:02 +0100

    compute error for all tests with a given setup

--- testing-frameworks/compute_error.py
+++ testing-frameworks/compute_error.py
@@ -1,4 +1,5 @@
 import sys
+import os  # for file handling functions
 from plot.Plot import Storage
 
 if __name__ == "__main__":
@@ -6,14 +7,15 @@ if __name__ == "__main__":
   # parsing command line parameters
   if len(sys.argv) != 2 :
     print('\033[91mError:\033[0m ' + 'python compute_error.py <1> ')
-    print('  <1>: absolute or relative path of the experiment file')
+    print('  <1>: name of testing setup [mitl,sitl,hitl,pitl]')
     exit()
 
-  file_location = sys.argv[1]
-  experiment_name = file_location.split('/')[-1]
-  data_storage = Storage()
-  data_storage.open(file_location, experiment_name)
-  print('* read data with total length: \033[33m' + str(data_storage.trace_length) + '\033[0m')
-
-
-  print(data_storage.computeControlError())
+  setup = sys.argv[1]
+  directory  = setup+'/flightdata/'
+  onlyfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+  for f in onlyfiles :
+    path = directory+f
+    data_storage = Storage()
+    data_storage.open(path,f)
+    # print(f)
+    print(data_storage.computeControlError())
