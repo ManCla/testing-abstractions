83dd8a9cad0d0ef35c84a27bbd86e99acef22b73
ManCla <claudio.mandrioli@gmail.com>
2023-01-23 10:16:02 +0100

    automatically iterate over the different setups

--- testing-frameworks/compute_error.py
+++ testing-frameworks/compute_error.py
@@ -3,19 +3,11 @@ import os  # for file handling functions
 from plot.Plot import Storage
 
 if __name__ == "__main__":
-
-  # parsing command line parameters
-  if len(sys.argv) != 2 :
-    print('\033[91mError:\033[0m ' + 'python compute_error.py <1> ')
-    print('  <1>: name of testing setup [mitl,sitl,hitl,pitl]')
-    exit()
-
-  setup = sys.argv[1]
-  directory  = setup+'/flightdata/'
-  onlyfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-  for f in onlyfiles :
-    path = directory+f
-    data_storage = Storage()
-    data_storage.open(path,f)
-    # print(f)
-    print(data_storage.computeControlError())
+  for setup in ['mitl','sitl','hitl','pitl']:
+    directory  = setup+'/flightdata/'
+    onlyfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    for f in onlyfiles :
+      path = directory+f
+      data_storage = Storage()
+      data_storage.open(path,f,silent=True)
+      print(setup+' '+f+' '+str(data_storage.computeControlError()))
