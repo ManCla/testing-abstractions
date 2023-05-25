c286b1168bc29e233e2c85afef1c4d3d17015d5b
ManCla <claudio.mandrioli@gmail.com>
2023-05-25 19:15:41 +1000

    by default do not inject bugs in mil

--- testing-frameworks/mitl_main.py
+++ testing-frameworks/mitl_main.py
@@ -8,7 +8,7 @@ import time
 from plot.Plot import Storage 
 
 # macros to inject bugs in controller
-initialPos   = True
+initialPos   = False
 gyroAxesSwap = False
 simUpdate    = False
 slowTick     = False
